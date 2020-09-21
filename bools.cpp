396349c82ef5c508abf7ddf28a94c4f40fb228b3
ollieyoung <ollieyoung1@hotmail.com>
2020-09-21 12:25:25 +0100

    Added very basic bool and escape sequences examples

---
+++ bools.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    bool pizza_is_good = true;
+    if (pizza_is_good) cout << std::boolalpha << pizza_is_good << std::endl;    //boolalpha outputs true or fales instead of 0 or 1
+}
---
+++ escape_seq.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+
+int main()
+{
+    cout << "Hello\tThere" << std::endl;
+    cout << "Hello\vThere" << std::endl;
+    cout << "Hello \"There\"" << std::endl;
+}
